c5ef21b7df376a7c470e8a81e633b5d0b1326b1c
John Engh <14243398+Frogdart5@users.noreply.github.com>
2021-10-14 18:03:00 -0500

    Added sample connector file

---
+++ src/SQLDatabaseConnection.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLDatabaseConnection {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://scrum-coke.database.windows.net:1433;"
+                        + "database=Student Services Database;"
+                        + "user=developer@scrum-coke;"
+                        + "password=Charge-Operator-Bush-Pupil-6;"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            // Code here.
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
