ef0b03b39a2d366f36c8225f44c0df6358752d86
John Engh <14243398+Frogdart5@users.noreply.github.com>
2021-11-11 10:34:29 -0600

    added variables to lisitng

--- src/main/java/Student_Services/Listing/listing.java
+++ src/main/java/Student_Services/Listing/listing.java
@@ -1,4 +1,16 @@
 package Student_Services.Listing;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class listing {
+    private String title;
+    private String description;
+    private int authorID;
+    private int likes;
+    private int imageID;
+    private LocalDateTime post_date;
+    private ArrayList<Integer> tags;
+    private int catID;
+
 }
