fee034be954700b6ef9a4086a2aa85f12d9ba7b1
frogdart5 <14243398+Frogdart5@users.noreply.github.com>
2021-11-21 19:47:26 -0600

    add get all listitngs

--- src/main/java/Student_Services/Listing/listingController.java
+++ src/main/java/Student_Services/Listing/listingController.java
@@ -3,6 +3,8 @@ package Student_Services.Listing;
 import Student_Services.Database.DBController;
 import Student_Services.Database.DBControllerSQLServer;
 
+import java.util.ArrayList;
+
 public class listingController {
     static DBController dbController = new DBControllerSQLServer("listings");
     public static boolean addListing(listing product) {
@@ -22,5 +24,9 @@ public class listingController {
     }
 
     public static boolean editListing(listing Product) { return dbController.editListing(Product); }
+
+    public static ArrayList<Integer> getAllListingIDs() {
+        return dbController.getAllListingIDs();
+    }
 
 }
